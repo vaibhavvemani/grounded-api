560b076925de31f7f78375afdec7203546cd5531
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-17 21:04:52 +0530

    removed waitress

--- api/app.py
+++ api/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from waitress import serve
 
 app = Flask(__name__)
 
 @app.route("/")
-def app():
+def home():
     return "Hello fella"
 
-
-if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=12112)
+@app.route('/about')
+def about():
+    return "About"
