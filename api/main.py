144993b7c29aa23a5a7ffc679808683b3a961f44
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-17 20:14:49 +0530

    More debugging

--- api/main.py
+++ api/main.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def test():
     return "Hello fella"
 
 
-if __name__ == '__main__':
-    app.run()
