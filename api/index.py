4e6460c59a1bdc935ee9f787597cc0ac693a8142
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-21 11:15:56 +0530

    unloading dependacies to reduce edge function size

--- api/index.py
+++ api/index.py
@@ -4,8 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello fella"
+    return """<h1>Welcome to Grounded!</h1> 
+    <ul>
+        <li>/: homepage </li>
+        <li>/pdf: RAG with a pdf </li>
+        <li>/webpage: RAG with a webpage </li>
+        <li>/testcase: Using RAG to build testcases </li>
+        <li>/sql: Using RAG to create SQL queries </li>
+    </ul>"""
 
-@app.route('/about')
-def about():
-    return "About"
+@app.route('/pdf')
+def pdf():
+    return "pdf"
+
+@app.route('/webpage')
+def webpage():
+    return "webpage"
+
+@app.route('/testcase')
+def testcase():
+    return "testcase"
+
+@app.route('/sql')
+def sql():
+    return "sql"
